d685c2c1fc0f5c0e7c64fbca6f2313b273965795
Tadavarti Rama Sujith <tadavartirama.s@ad.infosys.com>
2020-09-24 17:48:41 +0530

    commit to master

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 def git_opeation():
- print("I am adding example.py file to the remote repository.")
- print("This is for testing the visual studio")
- print("This is the third line to test")
+    print("I am adding example.py file to the remote repository.")
+    print("This is for testing the visual studio")
+    print("This is the third line to test")
+
+
 git_opeation()
